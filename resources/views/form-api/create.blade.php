4ce7643ded56bfbb5806194d3b9afa342fdc2aa2
rizky-gumelar <111202113304@mhs.dinus.ac.id>
2025-02-16 22:32:05 +0700

    set padding

--- resources/views/form-api/create.blade.php
+++ resources/views/form-api/create.blade.php
@@ -3,7 +3,7 @@
 
     <div class="bg-gradient-to-r from-blue-50 to-green-50 min-h-screen">
 
-        <div class="flex items-center space-x-2 sm:px-6 lg:px-8 max-w-7xl mx-auto py-12 ">
+        <div class="flex items-center space-x-2 sm:px-6 lg:px-8 max-w-7xl mx-auto py-6">
             <a href="{{ route('dashboard') }}" class="flex items-center text-gray-600 hover:text-gray-900">
                 <!-- Arrow Icon -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
+
+    <div class="bg-gradient-to-r from-blue-50 to-green-50 ">
+        <div class="flex items-center space-x-2 sm:px-6 lg:px-8 max-w-7xl mx-auto py-6 ">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Profile') }}
+            </h2>
+        </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
